493d34caebb2eca0b0588e54c4808f081198239a
Steven <steven@MacBook-Neo.local>
2026-08-31 16:21:23 +0800

    fix: resolve time module name collision

--- glados.py
+++ glados.py
@@ -25,13 +25,12 @@ if __name__ == '__main__':
         checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
         state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
     #--------------------------------------------------------------------------------------------------------#  
-        time = state.json()['data']['leftDays']
-        time = time.split('.')[0]
+        days_left = state.json()['data']['leftDays'].split('.')[0]
         email = state.json()['data']['email']
         if 'message' in checkin.text:
             mess = checkin.json()['message']
-            print(email+'----结果--'+mess+'----剩余('+time+')天')  # 日志输出
-            sendContent += email+'----'+mess+'----剩余('+time+')天\n'
+            print(f'{email}----结果--{mess}----剩余({days_left})天')  # 日志输出
+            sendContent += f'{email}----{mess}----剩余({days_left})天\n'
         else:
             print(f'{email} cookie已失效')
             sendContent += f'{email} cookie已失效\n'
